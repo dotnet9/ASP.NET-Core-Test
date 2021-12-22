e776fc8d82a7ed135b2986dfe608c5f6183a3f0b
liushiquan <1012434131@qq.com>
2021-12-22 19:56:02 +0800

    add authorization;

--- src/Acme.BookStore/src/Acme.BookStore.Application/Books/BookAppService.cs
+++ src/Acme.BookStore/src/Acme.BookStore.Application/Books/BookAppService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Acme.BookStore.Permissions;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
@@ -11,12 +12,16 @@ public class BookAppService :
 		BookDto, // Used to show books
 		Guid, // Primary key of the book entity
 		PagedAndSortedResultRequestDto, // Used for paging/sorting
-		CreateUpdateBookDto // Used to create/update a book
-	>,
+		CreateUpdateBookDto>, // Used to create/update a book
 	IBookAppService // implement the IBookAppService
 {
 	public BookAppService(IRepository<Book, Guid> repository)
 		: base(repository)
 	{
+		GetPolicyName = BookStorePermissions.Books.Default;
+		GetListPolicyName = BookStorePermissions.Books.Default;
+		CreatePolicyName = BookStorePermissions.Books.Create;
+		UpdatePolicyName = BookStorePermissions.Books.Edit;
+		DeletePolicyName = BookStorePermissions.Books.Delete;
 	}
 }
