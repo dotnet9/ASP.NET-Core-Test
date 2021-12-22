c1b52967541440bc70cb6a4581d15f11f70df92d
liushiquan <1012434131@qq.com>
2021-12-22 14:57:18 +0800

    add TodoItem entity;

--- src/AbpWithBlazorServer/src/TodoApp.EntityFrameworkCore/EntityFrameworkCore/TodoAppDbContext.cs
+++ src/AbpWithBlazorServer/src/TodoApp.EntityFrameworkCore/EntityFrameworkCore/TodoAppDbContext.cs
@@ -15,72 +15,67 @@ using Volo.Abp.TenantManagement.EntityFrameworkCore;
 
 namespace TodoApp.EntityFrameworkCore
 {
-    [ReplaceDbContext(typeof(IIdentityDbContext))]
-    [ReplaceDbContext(typeof(ITenantManagementDbContext))]
-    [ConnectionStringName("Default")]
-    public class TodoAppDbContext : 
-        AbpDbContext<TodoAppDbContext>,
-        IIdentityDbContext,
-        ITenantManagementDbContext
-    {
-        /* Add DbSet properties for your Aggregate Roots / Entities here. */
-        
-        #region Entities from the modules
-        
-        /* Notice: We only implemented IIdentityDbContext and ITenantManagementDbContext
-         * and replaced them for this DbContext. This allows you to perform JOIN
-         * queries for the entities of these modules over the repositories easily. You
-         * typically don't need that for other modules. But, if you need, you can
-         * implement the DbContext interface of the needed module and use ReplaceDbContext
-         * attribute just like IIdentityDbContext and ITenantManagementDbContext.
-         *
-         * More info: Replacing a DbContext of a module ensures that the related module
-         * uses this DbContext on runtime. Otherwise, it will use its own DbContext class.
-         */
-        
-        //Identity
-        public DbSet<IdentityUser> Users { get; set; }
-        public DbSet<IdentityRole> Roles { get; set; }
-        public DbSet<IdentityClaimType> ClaimTypes { get; set; }
-        public DbSet<OrganizationUnit> OrganizationUnits { get; set; }
-        public DbSet<IdentitySecurityLog> SecurityLogs { get; set; }
-        public DbSet<IdentityLinkUser> LinkUsers { get; set; }
-        
-        // Tenant Management
-        public DbSet<Tenant> Tenants { get; set; }
-        public DbSet<TenantConnectionString> TenantConnectionStrings { get; set; }
+	[ReplaceDbContext(typeof(IIdentityDbContext))]
+	[ReplaceDbContext(typeof(ITenantManagementDbContext))]
+	[ConnectionStringName("Default")]
+	public class TodoAppDbContext :
+		AbpDbContext<TodoAppDbContext>,
+		IIdentityDbContext,
+		ITenantManagementDbContext
+	{
+		/* Add DbSet properties for your Aggregate Roots / Entities here. */
+		public DbSet<TodoItem> TodoItems { get; set; }
 
-        #endregion
-        
-        public TodoAppDbContext(DbContextOptions<TodoAppDbContext> options)
-            : base(options)
-        {
+		#region Entities from the modules
 
-        }
+		/* Notice: We only implemented IIdentityDbContext and ITenantManagementDbContext
+		 * and replaced them for this DbContext. This allows you to perform JOIN
+		 * queries for the entities of these modules over the repositories easily. You
+		 * typically don't need that for other modules. But, if you need, you can
+		 * implement the DbContext interface of the needed module and use ReplaceDbContext
+		 * attribute just like IIdentityDbContext and ITenantManagementDbContext.
+		 *
+		 * More info: Replacing a DbContext of a module ensures that the related module
+		 * uses this DbContext on runtime. Otherwise, it will use its own DbContext class.
+		 */
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
+		//Identity
+		public DbSet<IdentityUser> Users { get; set; }
+		public DbSet<IdentityRole> Roles { get; set; }
+		public DbSet<IdentityClaimType> ClaimTypes { get; set; }
+		public DbSet<OrganizationUnit> OrganizationUnits { get; set; }
+		public DbSet<IdentitySecurityLog> SecurityLogs { get; set; }
+		public DbSet<IdentityLinkUser> LinkUsers { get; set; }
 
-            /* Include modules to your migration db context */
+		// Tenant Management
+		public DbSet<Tenant> Tenants { get; set; }
+		public DbSet<TenantConnectionString> TenantConnectionStrings { get; set; }
 
-            builder.ConfigurePermissionManagement();
-            builder.ConfigureSettingManagement();
-            builder.ConfigureBackgroundJobs();
-            builder.ConfigureAuditLogging();
-            builder.ConfigureIdentity();
-            builder.ConfigureIdentityServer();
-            builder.ConfigureFeatureManagement();
-            builder.ConfigureTenantManagement();
+		#endregion
 
-            /* Configure your own tables/entities inside here */
+		public TodoAppDbContext(DbContextOptions<TodoAppDbContext> options)
+			: base(options)
+		{
+		}
 
-            //builder.Entity<YourEntity>(b =>
-            //{
-            //    b.ToTable(TodoAppConsts.DbTablePrefix + "YourEntities", TodoAppConsts.DbSchema);
-            //    b.ConfigureByConvention(); //auto configure for the base class props
-            //    //...
-            //});
-        }
-    }
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			/* Include modules to your migration db context */
+
+			builder.ConfigurePermissionManagement();
+			builder.ConfigureSettingManagement();
+			builder.ConfigureBackgroundJobs();
+			builder.ConfigureAuditLogging();
+			builder.ConfigureIdentity();
+			builder.ConfigureIdentityServer();
+			builder.ConfigureFeatureManagement();
+			builder.ConfigureTenantManagement();
+
+			/* Configure your own tables/entities inside here */
+
+			builder.Entity<TodoItem>(b => { b.ToTable("TodoItems"); });
+		}
+	}
 }
