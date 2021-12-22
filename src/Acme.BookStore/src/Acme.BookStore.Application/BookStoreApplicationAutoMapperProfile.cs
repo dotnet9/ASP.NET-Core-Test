1af0a34618d89bec9e8bb9de85be212286d1e6e2
liushiquan <1012434131@qq.com>
2021-12-22 17:16:44 +0800

    add Book service;

--- src/Acme.BookStore/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ src/Acme.BookStore/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,14 +1,13 @@
-﻿using AutoMapper;
+﻿using Acme.BookStore.Books;
+using AutoMapper;
 
-namespace Acme.BookStore
+namespace Acme.BookStore;
+
+public class BookStoreApplicationAutoMapperProfile : Profile
 {
-    public class BookStoreApplicationAutoMapperProfile : Profile
-    {
-        public BookStoreApplicationAutoMapperProfile()
-        {
-            /* You can configure your AutoMapper mapping configuration here.
-             * Alternatively, you can split your mapping configurations
-             * into multiple profile classes for a better organization. */
-        }
-    }
+	public BookStoreApplicationAutoMapperProfile()
+	{
+		CreateMap<Book, BookDto>();
+		CreateMap<CreateUpdateBookDto, Book>();
+	}
 }
