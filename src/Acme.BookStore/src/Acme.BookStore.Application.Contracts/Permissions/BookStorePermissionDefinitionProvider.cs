2b5991867d093a44c8e7ce20a853df47baa9dc1a
liushiquan <1012434131@qq.com>
2021-12-22 19:50:49 +0800

    define permissions;

--- src/Acme.BookStore/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissionDefinitionProvider.cs
+++ src/Acme.BookStore/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissionDefinitionProvider.cs
@@ -2,20 +2,22 @@
 using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.Localization;
 
-namespace Acme.BookStore.Permissions
-{
-    public class BookStorePermissionDefinitionProvider : PermissionDefinitionProvider
-    {
-        public override void Define(IPermissionDefinitionContext context)
-        {
-            var myGroup = context.AddGroup(BookStorePermissions.GroupName);
-            //Define your own permissions here. Example:
-            //myGroup.AddPermission(BookStorePermissions.MyPermission1, L("Permission:MyPermission1"));
-        }
+namespace Acme.BookStore.Permissions;
 
-        private static LocalizableString L(string name)
-        {
-            return LocalizableString.Create<BookStoreResource>(name);
-        }
-    }
+public class BookStorePermissionDefinitionProvider : PermissionDefinitionProvider
+{
+	public override void Define(IPermissionDefinitionContext context)
+	{
+		var bookStoreGroup = context.AddGroup(BookStorePermissions.GroupName, L("Permission:BookStore"));
+
+		var booksPermission = bookStoreGroup.AddPermission(BookStorePermissions.Books.Default, L("Permission:Books"));
+		booksPermission.AddChild(BookStorePermissions.Books.Create, L("Permission:Books.Create"));
+		booksPermission.AddChild(BookStorePermissions.Books.Edit, L("Permission:Books.Edit"));
+		booksPermission.AddChild(BookStorePermissions.Books.Delete, L("Permission:Books.Delete"));
+	}
+
+	private static LocalizableString L(string name)
+	{
+		return LocalizableString.Create<BookStoreResource>(name);
+	}
 }
--- src/Acme.BookStore/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ src/Acme.BookStore/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -1,10 +1,14 @@
-﻿namespace Acme.BookStore.Permissions
-{
-    public static class BookStorePermissions
-    {
-        public const string GroupName = "BookStore";
+﻿namespace Acme.BookStore.Permissions;
 
-        //Add your own permission names. Example:
-        //public const string MyPermission1 = GroupName + ".MyPermission1";
-    }
+public static class BookStorePermissions
+{
+	public const string GroupName = "BookStore";
+
+	public static class Books
+	{
+		public const string Default = GroupName + ".Books";
+		public const string Create = Default + ".Create";
+		public const string Edit = Default + ".Edit";
+		public const string Delete = Default + ".Delete";
+	}
 }
