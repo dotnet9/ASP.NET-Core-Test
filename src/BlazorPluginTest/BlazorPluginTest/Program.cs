9d034cf46357bd5033eb01d685ba20764a27e4b7
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-04-11 20:43:08 +0800

    add dynamic route;

--- src/BlazorPluginTest/BlazorPluginTest/Program.cs
+++ src/BlazorPluginTest/BlazorPluginTest/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using BlazorPluginTest.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
 
 var app = builder.Build();
 
