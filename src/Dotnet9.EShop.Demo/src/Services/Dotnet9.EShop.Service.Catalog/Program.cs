26808b602ee6e42ff12db3d87d1d5a140ee2bdc4
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-03-31 17:15:24 +0800

    添加最小api测试；

---
+++ src/Dotnet9.EShop.Demo/src/Services/Dotnet9.EShop.Service.Catalog/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.AddServices();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ src/Dotnet9.EShop.Demo/src/Services/Dotnet9.EShop.Service.Catalog/Services/HealthService.cs
@@ -0,0 +1,7 @@
+﻿namespace Dotnet9.EShop.Service.Catalog.Services
+{
+    public class HealthService : ServiceBase
+    {
+        public IResult Get() => Results.Ok("success");
+    }
+}
